ef99b807c496a918de686b263edcce4c847f8d73
krygier-afk <krygier91@gmail.com>
2025-10-24 11:49:58 +0200

    feat: implementiere Basiscode (v0.1) und Regeln

---
+++ toolkit.py
@@ -0,0 +1,9 @@
+VERSION = "0.1"
+
+def banner():
+    # >>> KONFLIKTBEREICH: Hier werden zwei Personen
+    # gleichzeitig ändern <<<
+    return "Mini-Toolkit v0.1 Welcome"
+
+if __name__ == "__main__":
+    print(banner())
